490edbaa301d91de7eb5f1504d17e60a2c621aff
akshata <akshata2018p@gamil.com>
2023-03-04 15:00:29 +0530

    [akshata]Add:Create Stack

---
+++ StackQueueProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome to Stack Program in Data Structures");
+                Console.WriteLine("Please select as per the below given options :\n" +
+                    "Press 1: Creating custom stack using LinkedList.\n ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        StackQueue stackObject = new StackQueue();
+                        stackObject.Push(70);
+                        stackObject.Push(30);
+                        stackObject.Push(56);
+                        stackObject.Display();
+                        break;
+                    
+                   
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ StackQueueProblem/StackQueue.cs
@@ -0,0 +1,54 @@
+﻿using StackQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackQueue
+{
+    public class StackQueue
+    {
+        public Node top;
+        public StackQueue()
+        {
+            this.top = null;
+        }
+
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine(data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("Stack is ");
+            if (temp == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        internal void Push(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
